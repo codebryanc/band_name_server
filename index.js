a7de87ce11a93f49150a1f2f977be1302c431ed0
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2022-12-28 00:04:28 -0500

    UI:MOB: node back end server socket io

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 // [Properties]
 const express = require('express');
-const app = express();
+const path = require('path');
+require('dotenv').config();
 
+// [APP de express]
+const app = require('express')();
 
+// [Node server]
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
+// [Sockets Messages]
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => { 
+        console.log('Cliente desconectado');
+    });
+});
+
+// [Public path]
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
 
 // [Constructor]
-app.listen(3000, (err) => {
+server.listen(process.env.PORT, (err) => {
     if(err) {
         throw new Error(err);
     }
 
-    console.log(`Servidor corriendo en puerto: ${3000}`);
+    console.log(`Servidor corriendo en puerto: ${process.env.PORT}`);
 });
