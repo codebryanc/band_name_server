852c129699b18ff0a91a6ddcd8fc700675e49a56
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2022-12-28 12:48:12 -0500

    UI:MOB:FLUTTER: Configure new message to device

--- sockets/socket.js
+++ sockets/socket.js
@@ -13,5 +13,11 @@ io.on('connection', client => {
 
         io.emit('message', { admin: 'New message' });
     });
-    
+
+    client.on('emitir-mensaje', ( payload ) => {
+        // Esto emite a todos los clientes conectados
+        // io.emit('nuevo-mensaje', payload);
+        // Si lo quiero emitir a todos menos al que lo emitio es:
+        client.broadcast.emit('nuevo-mensaje', payload);
+    });
 });
